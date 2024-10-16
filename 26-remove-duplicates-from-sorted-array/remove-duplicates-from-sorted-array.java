83cfb699fdefed64284ad5f0cd14d16268706cee
raju01 <32497797+raju01@users.noreply.github.com>
2024-10-16 17:11:51 +0530

    Time: 1 ms (67.78%) | Memory: 44.7 MB (43.45%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -7,9 +7,14 @@ class Solution {
                 nums[index++] = nums[i+1];
             }
         }
-        for(int i=index;i<len;i++){
-            nums[index]=0;
-        }
+       
+        /**
+        * Below code is not needed as It does not matter
+        * what you leave beyond the returned k (hence they are underscores).
+        * for(int i=index;i<len;i++){
+        *    nums[index]=0;
+        * }
+        **/
         return index;
     }
 }
