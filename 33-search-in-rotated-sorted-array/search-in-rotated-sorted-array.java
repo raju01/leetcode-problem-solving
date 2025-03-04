8a9b3f086bce9128ec20c429ceb16553071046e7
raju01 <32497797+raju01@users.noreply.github.com>
2025-03-04 17:31:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (19.20%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -8,13 +8,13 @@ class Solution {
               return mid;
           //First part is sorted    
         if(nums[low]<=nums[mid]){
-         if(nums[low]<=target && target<=nums[mid]){
+         if(nums[low]<=target && target<nums[mid]){
             high=mid-1;
          }else{
             low=mid+1;
          }
         }else{  
-            if(target>=nums[mid] && target<=nums[high]){
+            if(target>nums[mid] && target<=nums[high]){
                 low=mid+1;
             }else{
                 high=mid-1;
